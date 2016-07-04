24c3b85949af9687f6e71793acd5510266e9339f
Matthew Gertner <matthew@salsitasoft.com>
2016-07-04 16:48:26 +0100

    Implemented deep clone of target element

--- examples/simple.jsx
+++ examples/simple.jsx
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom';
 import Beacon from '../src/Beacon';
 
 function renderBlock(id) {
-  const style = {
-    height: '100px',
-    width: '100px',
-    margin: '10px',
-    position: 'relative',
-    left: '10px',
-    backgroundColor: 'red'
-  };
-  return (<div style={style}>{id}</div>);
+  return (
+    <div style={{
+      height: '100px',
+      width: '100px',
+      margin: '10px',
+      position: 'relative',
+      left: '100px',
+      backgroundColor: 'red'
+    }}>
+      <div className="blue" style={{
+        width: '50px',
+        height: '25px',
+        margin: '0 0 0 25px'
+      }}>{id}</div>
+    </div>
+  );
 }
 
 class TestChild extends React.Component { // eslint-disable-line react/no-multi-comp
